dd00ad61c0fe6b033de8aba18d3b0cb88384660f
Mahidul Islam <mahidulislamtamim@gmail.com>
2025-05-14 19:05:51 +0600

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,25 +3,41 @@ import random
 choices = ("r", "p", "s")
 emojis = {"r" : "Rock", "p" : "Paper", "s" : "Scissor"}
 
-while True:
-    
-    user_choice = str(input("Rock, Paper or Scissor?(r/p/s):"))
-    if user_choice not in choices:
-        print("Invalid choice!")
-    else:
-        computer_choice = random.choice(choices)
-        print(f"Your chose {emojis[user_choice]}")
-        print(f"Computer chose {emojis[computer_choice]}")
+def get_user_choice():
+    while True:
+        user_choice = str(input("Rock, Paper or Scissor?(r/p/s):"))
+        if user_choice in choices:
+            return user_choice
+        else:
+            print("Invalid choice!")
 
-        if user_choice == computer_choice:
-            print("Tie!")
-        elif (
-            (user_choice == "r" and computer_choice == "s") or
-            (user_choice == "s" and computer_choice == "p") or
-            (user_choice == "p" and computer_choice == "r")):
-            print("You win")
-        else :
-            print("You lose") 
+
+def show_choices(user_choice, computer_choice):
+    print(f"Your chose {emojis[user_choice]}")
+    print(f"Computer chose {emojis[computer_choice]}")
+
+
+def determine_winer(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print("Tie!")
+    elif (
+        (user_choice == "r" and computer_choice == "s") or
+        (user_choice == "s" and computer_choice == "p") or
+        (user_choice == "p" and computer_choice == "r")):
+        print("You win")
+    else :
+        print("You lose") 
+
+def main():
+    while True:
+        user_choice = get_user_choice()    
+        computer_choice = random.choice(choices)
+
+        show_choices(user_choice, computer_choice)
+        
+        determine_winer(user_choice, computer_choice)
+
+        
 
         continue_game = (input("Do you play more?(y/n):")).lower()
         if continue_game == "n":
@@ -32,3 +48,4 @@ while True:
 
 
 
+main()
